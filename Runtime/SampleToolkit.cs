9943bb29ba06d1a92d87d11f1ceba88a5e3bf5cc
KyleNeubarth <kyle@neubarth.org>
2022-12-15 15:12:39 -0700

    added assembly definition

--- Runtime/SampleToolkit.cs
+++ Runtime/SampleToolkit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Kosm.tools
+namespace kosm.tools
 {
     public class SampleToolkit : MonoBehaviour
     {
